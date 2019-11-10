9dab30ec36c965d8ff07ad2629ce2132de56f5d2
Loban Rahman <loban@vrify.com>
2019-11-10 14:32:07 -0800

    Finished homework

--- chapter-3/exercise-3-2.ts
+++ chapter-3/exercise-3-2.ts
@@ -5,38 +5,35 @@ class StackNode<T> {
     ) {}
 }
 
-class Stack<T> {
+export class Stack<T> {
     topNode: StackNode<T> = null;
-    minNode: StackNode<T> = null;
 
-    push(val: T): Stack<T> {
+    push(val: T): this {
         this.topNode = new StackNode(val, this.topNode);
-
-        if (this.minNode === null || this.topNode.val < this.minNode.val)
-            this.minNode = this.topNode;
 
         return this;
     }
 
     pop(): T {
-        this._checkIfEmpty();
+        const val = this.top();
 
-        const val = this.topNode.val;
-        this.topNode = this.topNode.next;
+        this.drop();
 
         return val;
     }
 
-    min(): T {
-        this._checkIfEmpty();
-
-        return this.minNode.val;
-    }
-
     top(): T {
-        this._checkIfEmpty();
+        this.throwIfEmpty();
 
         return this.topNode.val;
+    }
+
+    drop(): this {
+        this.throwIfEmpty();
+
+        this.topNode = this.topNode.next;
+
+        return this;
     }
 
     isEmpty(): boolean {
@@ -44,25 +41,63 @@ class Stack<T> {
     }
 
     toString(): string {
-        let str = '{ stack = [';
+        let str = '[';
         for (let node = this.topNode; node !== null; node = node.next)
             str += node.val + ', ';
-        str += '], min = ' + this.minNode?.val + '] }';
+        str += ']';
 
         return str;
     }
 
-    private _checkIfEmpty() {
+    throwIfEmpty() {
         if (this.isEmpty())
             throw new Error('Stack is empty');
     }
 }
 
-const stack1 = new Stack();
+export class StackWithMin<T> extends Stack<T> {
+    minStack: Stack<T> = new Stack<T>();
+
+    push(val: T): this {
+        if (this.minStack.isEmpty() || val <= this.min())
+            this.minStack.push(val);
+
+        return super.push(val);
+    }
+
+    drop(): this {
+        if (this.top() == this.minStack.top())
+            this.minStack.drop();
+
+        return super.drop();
+    }
+
+    min(): T {
+        return !this.minStack.isEmpty() ? this.minStack.top() : undefined;
+    }
+
+    toString(): string {
+        return `{ stack = ${super.toString()}, min = ${this.min()} }`;
+    }
+}
+
+const stack1 = new StackWithMin();
 console.log(stack1.toString());
 
 stack1.push(13);
 console.log(stack1.toString());
 
-stack1.push(4).push(6).push(5).push(6).push(23);
+stack1.push(6).push(9).push(6).push(7).push(4).push(4).push(23);
+console.log(stack1.toString());
+
+stack1.drop().drop();
+console.log(stack1.toString());
+
+stack1.drop().drop();
+console.log(stack1.toString());
+
+stack1.drop().drop().drop();
+console.log(stack1.toString());
+
+stack1.drop();
 console.log(stack1.toString());
---
+++ chapter-3/exercise-3-3.ts
@@ -0,0 +1,87 @@
+import { Stack } from './exercise-3-2';
+
+class StackWithSize<T> extends Stack<T> {
+    length: number = 0;
+
+    push(val: T): this {
+        this.length++;
+
+        return super.push(val);
+    }
+
+    drop(): this {
+        this.length--;
+
+        return super.drop();
+    }
+}
+
+class SetOfStacks<T> {
+    stack = new Stack<StackWithSize<T>>();
+
+    constructor(public readonly maxLength: number = 3) {}
+
+    push(val: T): this {
+        if (this.isEmpty() || this.stack.top().length === this.maxLength)
+            this.stack.push(new StackWithSize<T>().push(val));
+        else
+            this.stack.top().push(val);
+
+        return this;
+    }
+
+    pop(): T {
+        const val = this.top();
+
+        this.drop();
+
+        return val;
+    }
+
+    top(): T {
+        return this.stack.top().top();
+    }
+
+    drop(): this {
+        this.throwIfEmpty();
+
+        this.stack.top().drop();
+        if (this.stack.top().isEmpty())
+            this.stack.drop();
+
+        return this;
+    }
+
+    isEmpty(): boolean {
+        return this.stack.isEmpty();
+    }
+
+    toString(): string {
+        return this.stack.toString();
+    }
+
+    protected throwIfEmpty() {
+        return this.stack.throwIfEmpty();
+    }
+}
+
+const stack1 = new SetOfStacks();
+console.log(stack1.toString());
+
+stack1.push(13);
+console.log(stack1.toString());
+
+stack1.push(6).push(9).push(6).push(7).push(4).push(4).push(23);
+console.log(stack1.toString());
+
+stack1.drop().drop();
+console.log(stack1.toString());
+
+stack1.drop().drop();
+console.log(stack1.toString());
+
+stack1.drop().drop().drop();
+console.log(stack1.toString());
+
+stack1.drop();
+console.log(stack1.toString());
