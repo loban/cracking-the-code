2e52517d85e19b9248e1692c30dcdc3fc91843f8
Loban Rahman <loban@vrify.com>
2019-12-10 23:42:46 -0800

    Session 2019-12-10

---
+++ chapter-8/exercise-8-4.ts
@@ -0,0 +1,31 @@
+function addElement<T>(letter: string, allSubwords: Set<string>): Set<string> {
+    Array.from(allSubwords.values()).forEach(subword => {
+        for (let i = 0; i <= subword.length; i++) {
+            const newSubword = subword.slice(subword.length - i) + letter + subword.slice(i);
+            allSubwords.add(newSubword);
+        }
+    });
+    return allSubwords;
+}
+
+function getAllPermutations<T>(word: string): Set<string> {
+    let allSubwords = new Set<string>(['']);
+    Array.from(word).forEach(letter => {
+        allSubwords = addElement(letter, allSubwords);
+    });
+    return allSubwords;
+}
+
+function test<T>(word: string) {
+    console.log('----------------------------------------');
+    console.log('word', word.length, word);
+    const permutations = getAllPermutations(word);
+    console.log('permutations', permutations.size, permutations);
+}
+
+test('');
+test('ab');
+test('aa');
+test('abc');
+test('aba');
+test('abcd');
